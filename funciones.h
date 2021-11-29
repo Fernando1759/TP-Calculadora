9792d046296c18842976a537e256d0b586918351
Fernando1759 <92633944+Fernando1759@users.noreply.github.com>
2021-11-28 22:14:42 -0300

    Add files via upload

--- funciones.h
+++ funciones.h
@@ -1,36 +1,6 @@
 #ifndef FUNCIONES_H_INCLUDED
 #define FUNCIONES_H_INCLUDED
 
-#ifndef FUNCIONES_H_INCLUDED
-#define FUNCIONES_H_INCLUDED
-
-
-
-int es_Numero(int numero);
-
-void menuOpciones(int op1, int op2);
-
-int getInt(char mensaje[]);
-
-int func_sumar(int op1, int op2);
-
-int func_restar(int op1, int op2);
-
-int func_dividir(int op1, int op2, float* direccionDeVariableDeRetorno);
-
-int func_multiplicar(int op1, int op2);
-
-int func_factorialDeX(int op1);
-
-
-
-
-
-
-
-#endif // FUNCIONES_H_INCLUDED
-
-
 
 
 #endif // FUNCIONES_H_INCLUDED
